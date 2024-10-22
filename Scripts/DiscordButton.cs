9da02235903439487326f07f11beac6d707fcba4
Hard working bee <beatrice.emma.soderqvist@gmail.com>
2024-10-22 15:34:53 +0200

    world map overlay

--- Scripts/DiscordButton.cs
+++ Scripts/DiscordButton.cs
@@ -3,16 +3,6 @@ using System;
 
 public class DiscordButton : Control
 {
-	// Declare member variables here. Examples:
-	// private int a = 2;
-	// private string b = "text";
-
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-		
-	}
-
 	private void _on_Control_gui_input(object @event)
 	{
 		if (@event is InputEventMouseButton mouseEvent && mouseEvent.Pressed)
