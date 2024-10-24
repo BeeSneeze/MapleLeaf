14f390fd049f7612683718165452535c87033497
Hard working bee <beatrice.emma.soderqvist@gmail.com>
2024-10-24 15:07:00 +0200

    Version 0.6

--- Export/index.service.worker.js
+++ Export/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1729690405|8454826792";
+const CACHE_VERSION = "1729775204|15286192138";
 const CACHE_PREFIX = "Maple Leaf-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
